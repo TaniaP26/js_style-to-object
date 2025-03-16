6f4971e6174bfbff892696746a9e873a929139cf
Tetiana Pyatachuk <pyatachuk26@gmail.com>
2025-03-16 17:48:05 +0200

    add task solution2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,18 +7,14 @@
  */
 function convertToObject(sourceString) {
   const result = {};
-  const arrSource = sourceString.split(';');
 
-  for (let i = 0; i < arrSource.length; i++) {
-    const resultItem = arrSource[i].split(':');
+  sourceString.split(';').forEach((pair) => {
+    const [key, value] = pair.split(':').map((str) => str.trim());
 
-    if (resultItem.length === 2) {
-      const key = resultItem[0].trim();
-      const value = resultItem[1].trim();
-
+    if (key && value) {
       result[key] = value;
     }
-  }
+  });
 
   return result;
 }
